c80b3198f4e3811b4aa70fd34b85dc4ff3cce8b8
Vinícius Fernando <vinicius.fd.ferreira835@gmail.com>
2026-06-18 10:13:59 -0300

    Create program to divide two integers with error handling
    Implement user input handling for division with error handling.

---
+++ aula 13/Exercício 1.java
@@ -0,0 +1,7 @@
+//Crie um programa que peça ao usuário dois números inteiros. O programa deve calcular e
+//exibir a divisão do primeiro pelo segundo. O seu código deve tratar as seguintes situações
+//usando try-catch:
+//○ O usuário digitar algo que não é um número inteiro.
+//○ O usuário tentar dividir por zero.
+//○ Caso ocorra um erro, exiba uma mensagem amigável e peça para o usuário tentar
+//novamente (use um loop para insistir até que funcione).
